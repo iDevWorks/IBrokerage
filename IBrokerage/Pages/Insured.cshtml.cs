4dc9dfe44d548e4900fdba3c0d079252e67e0c86
omomowo symeon <omomowosymeon45@gmail.com>
2023-12-07 17:20:05 +0100

    Add project files.

---
+++ IBrokerage/Pages/Insured.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IBrokerage.Pages
+{
+    public class InsuredModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
