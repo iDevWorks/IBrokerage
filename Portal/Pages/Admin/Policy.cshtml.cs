7429f26b1462f4d9143d681a705b8db760ae0dfc
omomowo symeon <omomowosymeon45@gmail.com>
2024-01-02 11:06:21 +0100

    update

---
+++ Portal/Pages/Admin/Policy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Gibs.Domain.Entities;
+using Gibs.Infrastructure.EntityFramework;
+
+namespace Gibs.Portal.Pages.Admin
+{
+    public class PolicyModel(IBrokerageContext context) : PageModel
+    {
+        public List<Policy> Policy { get; set; } = [];
+
+        public async Task<PageResult> OnGetAsync()
+        {
+            Policy = await context.Policies
+                .Include(p => p.Client)
+                .Include(p => p.IssuingCompany).ToListAsync();
+
+            return Page();
+        }
+    }
+}
