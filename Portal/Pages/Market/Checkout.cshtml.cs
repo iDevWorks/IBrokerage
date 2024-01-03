6853fb5b10656eea48f89f2443d614ddfb0d4ab1
omomowo symeon <omomowosymeon45@gmail.com>
2024-01-03 16:10:50 +0100

    market pages

---
+++ Portal/Pages/Market/Checkout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gibs.Portal.Pages.Market
+{
+    public class CheckoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Portal/Pages/Market/Products.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gibs.Portal.Pages.Market
+{
+    public class ProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
