eae98e53f143b99645bd23b01bb83042ef10f65e
omomowo symeon <omomowosymeon45@gmail.com>
2024-01-31 08:56:23 +0100

    update

--- Gibs.MultiBroker.Portal/Pages/Broker/Products/View.cshtml.cs
+++ Gibs.MultiBroker.Portal/Pages/Broker/Products/View.cshtml.cs
@@ -1,12 +1,80 @@
+using Gibs.Domain.Entities;
+using Gibs.Infrastructure.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gibs.Portal.Pages
 {
-    public class ProductViewModel : PageModel
+    public class ProductViewModel(BrokerContext context) : BrokerPageModel(context)
     {
-        public void OnGet()
+        [BindProperty(SupportsGet = true)]
+        public string Id { get; set; }
+
+        [BindProperty]
+        public string ShortName { get; set; }
+
+        [BindProperty]
+        public string ProductName { get; set; }
+
+        [BindProperty]
+        public string ClassId { get; set; }
+
+        public Product Product { get; set; }
+
+        public List<Order> ProductPurchases { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
         {
+            try
+            {
+                Product = await context.Products
+                    .SingleOrDefaultAsync(p => p.Id == Id)
+                    ?? throw new Exception("No product was found.");
+
+                ProductPurchases = await context.Orders
+                        .Where(o => o.Policies.Any(p => p.Product.Id == Id) 
+                        && o.PaymentStatus == OrderStatus.SUCCESS)
+                        .ToListAsync();
+
+                ProductName = Product.ProductName;
+                ShortName = Product.ShortName;
+                ClassId = Product.ClassId;
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostUpdateProductAsync()
+        {
+            if (!ModelState.IsValid)
+                return RedirectToPage();
+
+            try
+            {
+                var product = await context.Products.FindAsync(Id)
+                    ?? throw new Exception("product was not found");
+
+                // update product
+
+                await context.SaveChangesAsync();
+                ShowInfo("The product was updated successfully");
+                return RedirectToPage("Index");
+            }
+            catch (DbUpdateException ex)
+            {
+                ShowError(ex.Message);
+                return RedirectToPage();
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+                return RedirectToPage();
+            }
         }
     }
 }
--- Gibs.MultiBroker.Portal/Pages/Broker/Underwriters/View.cshtml.cs
+++ Gibs.MultiBroker.Portal/Pages/Broker/Underwriters/View.cshtml.cs
@@ -1,12 +1,68 @@
+using Gibs.Domain.Entities;
+using Gibs.Infrastructure.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gibs.Portal.Pages
 {
-    public class UnderwriterViewModel : PageModel
+    public class UnderwriterViewModel(BrokerContext context) : BrokerPageModel(context)
     {
-        public void OnGet()
+        [BindProperty(SupportsGet = true)]
+        public string ApiKeyUsername { get; set; }
+
+        public Underwriter Underwriter { get; private set; }
+
+        public List<Policy> UnderwriterPolicies { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
         {
+            try
+            {
+                Underwriter = await context.Underwriter
+                    .Include(u => u.Insurer)
+                    .SingleOrDefaultAsync(u => u.ApiKey1Username == ApiKeyUsername)
+                    ?? throw new Exception("No policy was found.");
+
+                UnderwriterPolicies =  await context.Policies
+                    .Include(p => p.Product)
+                    .Where(p => p.Insurer.Id == Underwriter.Insurer.Id)
+                    .ToListAsync()
+                    ?? throw new Exception("No policy was found for this insurer.");
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostUpdateUnderwriterAsync()
+        {
+            if (!ModelState.IsValid)
+                return RedirectToPage();
+
+            try
+            {
+                var underwriter = await context.Underwriter.FindAsync(ApiKeyUsername)
+                    ?? throw new Exception("Underwriter was not found");
+
+                // update underwriter
+
+                await context.SaveChangesAsync();
+                ShowInfo("The underwriter was updated successfully");
+                return RedirectToPage("Index");
+            }
+            catch (DbUpdateException ex)
+            {
+                ShowError(ex.Message);
+                return RedirectToPage();
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+                return RedirectToPage();
+            }
         }
     }
 }
